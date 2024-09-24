bad9525a046fd609f96e823ac95002032b3b3f1f
Akash Arul <113762183+21002624@users.noreply.github.com>
2024-09-24 21:29:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+function showInput(shape) {
+    // Hide all input sections
+    const inputSections = document.querySelectorAll('.input-section');
+    inputSections.forEach(section => {
+        section.style.display = 'none';
+    });
+
+    // Show the relevant input section based on the shape clicked
+    const input = document.getElementById(`input${shape.charAt(0).toUpperCase() + shape.slice(1)}`);
+    input.style.display = 'block';
+    console.log(`You clicked ${shape.charAt(0).toUpperCase() + shape.slice(1)}`);
+}
+
+
+function calculation(shape){
+    let inputValue;
+    let base,height;
+    let ans;
+    if(shape==='square'){
+        inputValue = parseFloat(document.getElementById('squareInput').value);
+        ans=inputValue*inputValue;
+        console.log(ans);
+    }
+    if(shape==='circle'){
+        inputValue = parseFloat(document.getElementById('circleInput').value);
+        ans=3.14*inputValue*inputValue;
+        console.log(ans);
+    }
+    if(shape==='triangle'){
+        base = parseFloat(document.getElementById('baseInput').value);
+        height = parseFloat(document.getElementById('heightInput').value);
+        ans=0.5*base*height;
+        console.log(ans);
+    }
+
+    const anss=document.getElementById('ansButton');
+    const areaOutput=document.getElementById('areaOutput');
+    
+    if(isNaN(ans)){
+        areaOutput.textContent=`Area is : Enter a Value`;
+    }
+    else{
+        areaOutput.textContent=`Area is : ${ans}`;
+    }
+    anss.style.display='block';
+
+}
